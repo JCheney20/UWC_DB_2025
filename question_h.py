cfa4814d5cefc64997868afbff30e132c87bf032
Lisa <136716760+lisabeeaan@users.noreply.github.com>
2025-08-03 15:44:25 +0200

    Update(Lisa) code for question_h.py

--- question_h.py
+++ question_h.py
@@ -1,3 +1,18 @@
+#Answering "List the country names that end with the letter ‘a’, without any repetitions."
 
 def solve(data: list[dict[str, str]]) -> str:
-    return "todo... :("
+    countries_ending_with_a = set() #Created a variable set to place the country names ending with 'a' (set () ignores duplicate values)
+
+    for row in data: #For loop to iterate through each row in the data
+        country_name = row['CountryName'].strip().lower()  #Get the country names and removes any leading and trailing whitespaces
+
+        if country_name.lower().endswith('a'): #Checks if the country name ends with 'a'
+            countries_ending_with_a.add(country_name)  #Adds to set
+
+    if not countries_ending_with_a:
+        return "No countries found that end with 'a'."
+    else:
+        sorted_countries = sorted(countries_ending_with_a) #sorts the countries alphabetically
+        countries_list = "\n".join(sorted_countries) #joins all items into one string
+        return countries_list #returns list of country names ending with a
+
