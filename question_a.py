602116f6dcfc70571038470b7930b64e7cbb1f32
Lisa <136716760+lisabeeaan@users.noreply.github.com>
2025-08-03 15:43:29 +0200

    Update(Lisa) code for question_a.py

--- question_a.py
+++ question_a.py
@@ -1,4 +1,12 @@
+#Answering "How many country names end with the letter 'a'?"
 
 def solve(data: list[dict[str, str]]) -> str:
-    # return data[0]["CountryName"]
-    return "todo... :("
+    countries_end_with_a = set() #Created a variable set to place the country names ending with 'a' (set () ignores duplicate values)
+
+    for row in data: #For loop to iterate through each row in the data
+        country_name = row['CountryName'].strip().lower()  #Get the country names and removes any leading and trailing whitespaces
+
+        if country_name.lower().endswith('a'): #Checks if the country name ends with 'a'
+            countries_end_with_a.add(country_name)  #Adds to the set
+
+    return f"{len(countries_end_with_a)}" #Returns number of items
