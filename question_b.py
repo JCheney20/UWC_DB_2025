b914e3e49cb98f51f56f6bb52e1e86970ff0731a
4323072 <4323072@myuwc.ac.za>
2025-08-05 00:25:17 +0200

    Added code for question_b

--- question_b.py
+++ question_b.py
@@ -1,3 +1,17 @@
 
 def solve(data: list[dict[str, str]]) -> str:
-    return "todo... :("
+    city_populations = {}  # Create a  to store the highest population for each city
+    
+    for row in data:  # Iterate through each row
+        city_name = row['CityName'].strip()
+        population = int(row['CityPopulation'])  
+        
+        # store only the highest population for each city, update if current population is higher
+        if city_name not in city_populations or population > city_populations[city_name]:
+            city_populations[city_name] = population
+    
+    # Sort cities by population 
+    top_cities = sorted(city_populations.items(), key=lambda x: x[1], reverse=True)[:5]
+    
+    # Return just the top 5 city names
+    return [city[0] for city in top_cities]
