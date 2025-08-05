3f3c422e987ba00cec76de6dcaf43d1d1a1d200c
manditembe03 <amandatembe13@icloud.com>
2025-08-05 03:13:55 +0200

    question_d.py

--- question_d.py
+++ question_d.py
@@ -1,3 +1,36 @@
+#The Hitch Hackers 4323284 question_d.py DB Practical 1
+
+#references:
+#https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
+#https://www.studytonight.com/python-howtos/count-unique-values-in-python-list
+#https://www.askpython.com/python/dictionary/filter-list-of-dictionaries-based-on-key-values
+
 
 def solve(data: list[dict[str, str]]) -> str:
-    return "todo... :("
+   
+    # "set" prevents counting the same country many times 
+    countries_60s80s = set()
+    
+    # goes over  each row in the data
+    for row in data:
+        indep_year = row['IndepYear']
+        country_name = row['CountryName']
+        
+        # skips the rows where IndepYear is NULL/empty
+        if indep_year == 'NULL' or indep_year == '':
+            continue
+            
+        try:
+            # converts the indep year to integer
+            year = int(indep_year)
+            
+            # checks if the year is between 1960 and 1980 
+            if 1960 <= year <= 1980:
+                countries_60s80s.add(country_name)
+
+        # skip the rows where IndepYear can't be converted to integer        
+        except ValueError:
+            continue
+    
+    # returns the string containing the count of countries
+    return str(len(countries_60s80s))
