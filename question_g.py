24ff624b438a182d97999fe723d06a10a75cf40b
JCheney20 <4323819@myuwc.ac.za>
2025-08-06 21:11:48 +0200

    merge():

--- question_g.py
+++ question_g.py
@@ -4,23 +4,28 @@
 # https://www.geeksforgeeks.org/python/python-increment-value-in-dictionary/
 
 # Helper func to get Language of a city
-def get_Lang(city):
+def getLang(city):
     return city["Language"]
 
 
 def solve(data: list[dict[str, str]]) -> str:
     answer = ""
     spokenLanguages = {}
+    vistedCountryLang = []
 
 # Calc num of cities that speak a language, inc if it is already there, add if not
     for city in data:
-        spokenLanguages[get_Lang(city)] = spokenLanguages.get(get_Lang(city), 0)+1
+        countryLang = f"{city["CountryName"]}{city["Language"]}"
+        if city["Language"] != "" and countryLang not in vistedCountryLang:
+            pertSpoken = (int(city["CountryPopulation"]) * (float(city["Percentage"])/100))
+            spokenLanguages[getLang(city)] = spokenLanguages.get(getLang(city), 0)+pertSpoken
+            vistedCountryLang.append(countryLang)
 
     # Sort the list in descending order
     sortedLanguages = sorted(spokenLanguages.items(), reverse=True, key=lambda lang: lang[1])[:5]
 
     # Add the top 5 to the answer string
     for language in sortedLanguages:
-        answer += f"{language[0]}\n"
+        answer += f"{language[0]} - {language[1]}\n"
 
     return answer
