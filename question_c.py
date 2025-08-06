becea1e591c6fb98080c74bc3811802a5e5f7b7c
4323072 <4323072@myuwc.ac.za>
2025-08-06 13:53:21 +0200

    Added code for question_c.py

--- question_c.py
+++ question_c.py
@@ -1,3 +1,7 @@
+#The Hitch Hackers
+#4323072
+#question_c
+#DB_Practical 1
 
 def solve(data: list[dict[str, str]]) -> list[str]:
     country_landmass = {}  # Create a variable set to store landmass for each country
